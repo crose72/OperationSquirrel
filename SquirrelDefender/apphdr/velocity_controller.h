505bc5ccb43428e5c8841e0c22fae24a832674a5
crose72 <cameroncrose72junk@gmail.com>
2024-06-02 20:03:50 -0400

    Updated bitmask for velocity control functions

--- SquirrelDefender/apphdr/velocity_controller.h
+++ SquirrelDefender/apphdr/velocity_controller.h
@@ -33,17 +33,17 @@ class VelocityController
         VelocityController(void);
         ~VelocityController(void);
 
-        static void cmd_position_NED(float position_target[3]);
-        static void cmd_velocity_NED(float velocity_target[3]);
-        static void cmd_acceleration_NED(float acceleration_target[3]);
-        static void cmd_velocity_x_NED(float velocity_target);
-        static void cmd_velocity_y_NED(float velocity_target);
-        static void cmd_velocity_z_NED(float velocity_target);
-        static void cmd_velocity_xy_NED(float velocity_target[3]);
+        void cmd_position_NED(float position_target[3]);
+        void cmd_velocity_NED(float velocity_target[3]);
+        void cmd_acceleration_NED(float acceleration_target[3]);
+        void cmd_velocity_x_NED(float velocity_target);
+        void cmd_velocity_y_NED(float velocity_target);
+        void cmd_velocity_z_NED(float velocity_target);
+        void cmd_velocity_xy_NED(float velocity_target[3]);
         
     private:
-        static float calc_yaw_target(float x, float y);
-        static float calc_yaw_rate_target(float x, float y);
+        float calc_yaw_target(float x, float y);
+        float calc_yaw_rate_target(float x, float y);
 };
 
 
