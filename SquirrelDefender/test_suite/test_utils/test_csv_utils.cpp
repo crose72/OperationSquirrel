33d7be534f7cd76077f79fd38298df1a8db8a17a
crose72 <cameroncrose72junk@gmail.com>
2025-07-30 22:34:10 -0400

    Added ability to dynamically choose test harness input file and output file

--- SquirrelDefender/test_suite/test_utils/test_csv_utils.cpp
+++ SquirrelDefender/test_suite/test_utils/test_csv_utils.cpp
@@ -32,27 +32,49 @@
  ********************************************************************************/
 
 /********************************************************************************
- * Function: 
- * Description: 
+ * Function:
+ * Description:
  ********************************************************************************/
 
 /********************************************************************************
  * Function: find_column_index
  * Description: Return the index of a column header in a csv file given the text
- *              in the column header (e.g. if "time" is header of column 5, 
+ *              in the column header (e.g. if "time" is header of column 5,
  *              return 5)
  ********************************************************************************/
-int find_column_index(const std::string& header, const std::string& column) 
+int find_column_index(const std::string &header, const std::string &column)
 {
     std::stringstream ss(header);
     std::string col;
     int column_index = 0;
 
-    while (std::getline(ss, col, ',')) 
+    while (std::getline(ss, col, ','))
     {
-        if (col == column) return column_index;
+        if (col == column)
+            return column_index;
         column_index++;
     }
 
     return -1; // Column not found
+}
+
+/********************************************************************************
+ * Function: generate_unique_filename
+ * Description: If file exists, append an incremented number to the file name.
+ ********************************************************************************/
+std::string generate_unique_filename(const std::string &data_file_path, const std::string &filename)
+{
+    int counter = 1;
+    std::string new_file_name = data_file_path + filename + ".csv"; // Add .csv extension initially
+    std::ifstream file(new_file_name);
+
+    while (file.is_open() == true)
+    {
+        file.close();
+        // If the file exists, append the counter number and try again
+        new_file_name = data_file_path + filename + "_" + std::to_string(counter++) + ".csv";
+        file.open(new_file_name);
+    }
+
+    return new_file_name;
 }
